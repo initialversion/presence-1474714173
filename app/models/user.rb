daedb8506f38420196e2f07b5b6dee444ca1bb95
Murugan <murugu2012@gmail.com>
2016-09-24 16:20:01 +0530

    Validate uniqueness of users.integer1

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,8 @@ class User < ApplicationRecord
 
   # Validations
 
+  validates :integer1, :uniqueness => { :scope => [:last_name, :integer3], :allow_blank => true, :case_sensitive => true, :message => "unique message" }
+
   validates :integer1, :length => { :minimum => 30, :maximum => 50, :allow_blank => true }
 
   validates :last_name, :presence => true
